6663dd9c8f68bc0a88b2360afee83589aaa6ab45
Brijesh Patil <brijesh@shoptrade.co>
2024-02-06 13:29:11 +0530

    minor updates in footer

--- assets/custom.js
+++ assets/custom.js
@@ -1,9 +1,26 @@
-function initializingFilter() { 
-    var dropdownList = document.querySelectorAll("[data-sort-list-label]");
-    console.log(dropdownList);
-    dropdownList.forEach((element) => console.log(element));
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    initializingFilter();
-});
+//     const dropdownBtn = document.querySelector('dropdown-opener.sort-list__btn');
+//     // console.log(dropdownBtn);
+//     // if (!dropdownBtn) return;
+
+//     dropdownBtn.addEventListener('click', () => {
+//         const dropdownList = document.querySelectorAll('[data-sort-list-label]');
+//         dropdownList.forEach(element => {
+//             element.addEventListener('click', () => {
+//                 const isCollectionPage = window.location.pathname.includes('/collections/');
+//                 const hasAvailabilityFilter = new URLSearchParams(window.location.search).has('filter.v.availability');
+//                 console.log(hasAvailabilityFilter);
+//                 if (isCollectionPage) {
+//                     const searchParams = window.location.search;
+//                     console.log(searchParams);
+//                     // const availableFilter = '&filter.v.availability=1';
+//                     searchParams.append(searchParams);
+//                     // searchParams.set('filter.v.availability', '1');
+//                     // window.location.search = searchParams.toString();
+//                 }
+//             });
+//         });
+//     });
+
+
+// // document.addEventListener('DOMContentLoaded', initializingFilter);
